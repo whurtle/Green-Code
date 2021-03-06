36f1f335d473da074042ce65663f0e2ee59e1965
Deborah Meda <dmeda@csumb.edu>
2021-03-05 19:56:31 -0800

    Revert "Merge pull request #3 from bitmonst3r/Debbie"
    This reverts commit ca2ef24d5cd2da5d7a7accb9142ed0f3aa96983e, reversing
    changes made to 4a23f39aadbbafc083b9f43c1a3a09b10fe8ed2f.

--- routes/Login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET login listing. */
-router.get('/', function(req, res, next) {
-  res.render('Login', { title: 'Green-Code' });
-});
-
-module.exports = router;
