86e1fa6da92908d3545a2aab1f0f5a9b0fc284cc
Deborah Meda <dmeda@csumb.edu>
2021-04-14 11:02:11 -0700

    changed text to long blob type

--- db_script.sql
+++ db_script.sql
@@ -17,7 +17,7 @@ CREATE TABLE Code(
 	userId int,
 	submissionNumber int,
 	submissionTimestamp date,
-	codeString longtext,
+	codeString longblob,
 
 	PRIMARY KEY(submissionId),
 	FOREIGN KEY(userId) REFERENCES User(userId) ON DELETE CASCADE
